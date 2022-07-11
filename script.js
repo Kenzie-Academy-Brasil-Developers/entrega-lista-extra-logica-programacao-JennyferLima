35cf5fd214007b69fdd8a92d72b52911faa80287
JennyferLima <ojennyferlima@gmail.com>
2022-07-11 17:37:01 -0300

    Correção item cashMachine

--- script.js
+++ script.js
@@ -32,31 +32,109 @@ function calculateSalary(salarioFixo, vendas){
 function cashMachine(valorSaque, salarioFixo, vendas){
 
     let saque = valorSaque;
-    
-    let cedulasDisponiveis = [200, 100, 50, 20, 10, 5, 2];
-
+        
     let saldoRestante = calculateSalary(salarioFixo, vendas) - valorSaque;
 
+    //let cedulasDisponiveis = [200, 100, 50, 20, 10, 5, 2];
+    let cedulasDisponiveis = [0, 0 , 0, 0, 0, 0, 0]
 
     if(valorSaque > calculateSalary(salarioFixo, vendas)){
         return `Valor insuficiente para saque.`
     }
-    
-    cedulasDisponiveis.forEach(elemento => {
-        let qtdNotas = parseInt(valorSaque/elemento);
 
-        if(qtdNotas > 0){
-            console.log(`Foram sacadas ${qtdNotas} notas de R$ ${elemento},00.`)
+    while(saque > 0){
+        if(saque >= 200){
+            cedulasDisponiveis[0]++;
+            saque -= 200;
         }
-        saque = saque % elemento;
+        else if(saque >= 100){
+            cedulasDisponiveis[1]++;
+            saque -= 100;
+        }
+        else if(saque >= 50){
+            cedulasDisponiveis[2]++;
+            saque -= 50;
+        }
+        else if(saque >= 20){
+            cedulasDisponiveis[3]++;
+            saque -= 20;
+        }
+        else if(saque >= 10){
+            cedulasDisponiveis[4]++;
+            saque -= 10;
+        }
+        else if(saque >= 5){
+            cedulasDisponiveis[5]++;
+            saque -= 5;
+        }
+        else if(saque >= 2){
+            cedulasDisponiveis[6]++;
+            saque -= 2;
+        }
 
-    })
-    console.log(`Portanto, o valor do saque foi ${saque} e o novo saldo é ${saldoRestante}`)
+    }
 
+    return `Foram sacadas ${cedulasDisponiveis[0]} notas de R$ 200, ${cedulasDisponiveis[1]} notas de R$ 100, ${cedulasDisponiveis[2]} notas de R$ 50, ${cedulasDisponiveis[3]} notas de R$ 20, ${cedulasDisponiveis[4]} notas de R$ 10, ${cedulasDisponiveis[5]} notas de R$ 5 e ${cedulasDisponiveis[6]} notas de R$ 2. Saldo restante é ${saldoRestante}`
 
 }
-cashMachine(4500, 15000, 6785)
-//console.log(cashMachine(4500, 15000, 6785))
+console.log(cashMachine(2500, 15000, 6785))
+    
+
+/*
+
+    OUTRA MANEIRA DE FAZER:
+
+    let nota200 = 0;
+    let nota100 = 0;
+    let nota50 = 0;
+    let nota20 = 0;
+    let nota10 = 0;
+    let nota5 = 0;
+    let nota2= 0;
+
+    if(valorSaque > calculateSalary(salarioFixo, vendas)){
+        return `Valor insuficiente para saque.`
+    }
+
+    while(saque > 0){
+        if(saque >= 200){
+            nota200++;
+            saque -= 200;
+            //console.log(saque, nota200)
+        }
+        else if(saque >= 100){
+            nota100++;
+            saque -= 100;
+        }
+        else if(saque >= 50){
+            nota50++;
+            saque -= 50;
+        }
+        else if(saque >= 20){
+            nota20++;
+            saque -= 20;
+        }
+        else if(saque >= 10){
+            nota10++;
+            saque -= 10;
+        }
+        else if(saque >= 5){
+            nota5++;
+            saque -= 5;
+        }
+        else if(saque >= 2){
+            nota2++;
+            saque -= 2;
+        }
+
+    }
+
+    return `Foram sacadas ${nota200} notas de R$ 200, ${nota100} notas de R$ 100, ${nota50} notas de R$ 50, ${nota20} notas de R$ 20, ${nota10} notas de R$ 10, ${nota5} notas de R$ 5 e ${nota2} notas de R$ 2. Saldo restante é ${saldoRestante}`
+
+}
+console.log(cashMachine(2500, 15000, 6785))
+
+*/
 
 
 
